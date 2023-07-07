c203c405c760749d7a6a65efae59f8c15f4bfecb
maha <maha@gmail.com>
2023-07-07 08:39:23 +0530

    chef

--- cookbooks/mytomonubuntu20/recipes/starttomcat.rb
+++ cookbooks/mytomonubuntu20/recipes/starttomcat.rb
@@ -1,14 +1,23 @@
 execute 'Reload the systemd daemon' do
     command 'sudo systemctl daemon-reload'
     action :run
+    not_if { ::File.exist?('/home/ubuntu/mytomstart') }
 end
 
 execute 'start the Tomcat service' do
     command 'sudo systemctl start tomcat'
     action :run
+    not_if { ::File.exist?('/home/ubuntu/mytomstart') }
 end
 
 execute 'enable Tomcat starting up with the system' do
     command 'sudo systemctl enable tomcat'
     action :run
+    not_if { ::File.exist?('/home/ubuntu/mytomstart') }
+end
+
+file '/home/ubuntu/mytomstart' do
+    content 'content'
+    mode '0755'
+    action :create
 end
